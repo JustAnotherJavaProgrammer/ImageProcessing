8397ab8e5aa42ad1b8f207bc12c81656b85cd127
JustAnotherJavaProgrammer <JustAnotherJavaProgrammer@users.noreply.github.com>
2021-09-26 01:27:05 +0200

    Optimized FilterRunnerParallel by removing parallelization on the x-axis

--- Bildverarbeitung/src/core/filters/FilterRunnerParallel.java
+++ Bildverarbeitung/src/core/filters/FilterRunnerParallel.java
@@ -6,8 +6,6 @@ import de.informatics4kids.Picture;
 
 /**
  * Parallel version of {@link FilterRunner}<br>
- * <b>Warning: </b>This version actually runs slower than the non-parallel
- * version, on my machine.
  */
 public class FilterRunnerParallel extends FilterRunner {
 	public static Picture applyFilter(Picture pic, Filter f) {
@@ -43,19 +41,42 @@ public class FilterRunnerParallel extends FilterRunner {
 		ImageViewMatrix view = new ImageViewMatrix(pic, width, height, edgeBehaviour);
 		IntStream.range(0, 3).parallel().forEach(colorLayer -> {
 			IntStream.range(0, pic.heightY()).parallel().forEach(y -> {
-				IntStream.range(0, pic.widthX()).parallel().forEach(x -> {
-					ImageViewMatrix localCopy = view.quickClone();
-					localCopy.setColorLayer(colorLayer);
+				ImageViewMatrix localCopy = view.quickClone();
+				localCopy.setColorLayer(colorLayer);
+				// Not using a parallel version of this part actually makes it faster
+				for (int x = 0; x < pic.widthX(); x++) {
 					localCopy.setPosition(x, y);
-					int res = (int) filters[colorLayer % filters.length].transform(localCopy);
 					try {
-						ImageViewMatrix.setColorLayerValue(result, x, y, colorLayer, res);
+						ImageViewMatrix.setColorLayerValue(result, x, y, colorLayer,
+								(int) filters[colorLayer % filters.length].transform(localCopy));
 					} catch (ArrayIndexOutOfBoundsException e) {
 						System.err.println("Error at (" + x + ", " + y + ")");
 					}
-				});
+				}
+				;
 			});
 		});
 		return result;
 	}
+
+//	public static Picture applyFilter(Picture pic, Filter[] filters, int width, int height, int edgeBehaviour) {
+//		Picture result = new Picture(pic.widthX(), pic.heightY());
+//		ImageViewMatrix view = new ImageViewMatrix(pic, width, height, edgeBehaviour);
+//		IntStream.range(0, 3).parallel().forEach(colorLayer -> {
+//			IntStream.range(0, pic.heightY()).parallel().forEach(y -> {
+//				IntStream.range(0, pic.widthX()).parallel().forEach(x -> {
+//					ImageViewMatrix localCopy = view.quickClone();
+//					localCopy.setColorLayer(colorLayer);
+//					localCopy.setPosition(x, y);
+//					try {
+//						ImageViewMatrix.setColorLayerValue(result, x, y, colorLayer,
+//								(int) filters[colorLayer % filters.length].transform(localCopy));
+//					} catch (ArrayIndexOutOfBoundsException e) {
+//						System.err.println("Error at (" + x + ", " + y + ")");
+//					}
+//				});
+//			});
+//		});
+//		return result;
+//	}
 }
